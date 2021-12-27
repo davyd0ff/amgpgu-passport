589d4c88f73315e9f562c5dfaae2bfac830ff81f
Ark Davydov <ark.davyd0ff@gmail.com>
2021-12-27 15:26:55 +1000

    repair store/student/mutations & tests

--- resources/js/__tests__/unit/store/student/mutations.spec.js
+++ resources/js/__tests__/unit/store/student/mutations.spec.js
@@ -2,11 +2,13 @@ import mutations from '@/store/student/mutations';
 
 describe('test student mutations', () => {
   it('SET_STUDENT', () => {
-    let state = {};
-    const student = 'TEST';
+    const state = {
+      info: {},
+    };
+    const student = { name: 'TEST' };
 
     mutations.SET_STUDENT(state, { student });
 
-    expect(state.student).toBe(student);
+    expect(state.info).toStrictEqual(student);
   });
 });
