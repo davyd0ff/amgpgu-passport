6154cb473533d86f8088b85a1db1c8eec0f9268d
Ark Davydov <ark.davyd0ff@gmail.com>
2021-12-27 16:05:10 +1000

    repair test: delete login & logout, becouse getters was moved to auth.getters

--- resources/js/__tests__/unit/store/user/getters.spec.js
+++ resources/js/__tests__/unit/store/user/getters.spec.js
@@ -1,34 +1,3 @@
 import getters from '@/store/user/getters.js';
 
-describe("store: user's getters", () => {
-  describe('isAuthenticated', () => {
-    it('is truthy', () => {
-      const state = {};
-      const token = { token: { access: 'TEST_ACCESS_TOKEN' } };
-      window.localStorage.__proto__.getItem = jest.fn(() =>
-        JSON.stringify(token)
-      );
-
-      const isAuthenticated = getters.isAuthenticated(state);
-
-      expect(isAuthenticated).toBeTruthy();
-      expect(window.localStorage.__proto__.getItem).toHaveBeenCalledTimes(1);
-      expect(window.localStorage.__proto__.getItem).toHaveBeenCalledWith(
-        'token'
-      );
-    });
-
-    it('is falsy', () => {
-      const state = {};
-      window.localStorage.__proto__.getItem = jest.fn(() => null);
-
-      const isAuthenticated = getters.isAuthenticated(state);
-
-      expect(isAuthenticated).toBeFalsy();
-      expect(window.localStorage.__proto__.getItem).toHaveBeenCalledTimes(1);
-      expect(window.localStorage.__proto__.getItem).toHaveBeenCalledWith(
-        'token'
-      );
-    });
-  });
-});
+describe('store/user/getters', () => {});
