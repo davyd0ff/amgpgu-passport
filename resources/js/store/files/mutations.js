2f52dcb8929b8f3593b84c12437b06ecebcc4a31
Ark Davydov <ark.davyd0ff@gmail.com>
2021-12-27 16:06:03 +1000

    repair test: state.files was changed to only state

--- resources/js/store/files/mutations.js
+++ resources/js/store/files/mutations.js
@@ -2,21 +2,15 @@ export default {
   ADD_FILES: (state, { context, files }) => {
     // console.log('store.files.mutations.addFiles()', context, files);
     // todo develop: save/replace files-info into storage
-    state = {
-      ...state,
-      [context]: [...state[context], ...files],
-    };
+    state[context] = [...state[context], ...files];
   },
 
   LOAD_FILES: (state, { context, files }) => {
     // todo develop: save/replace files-info into storage
-    state = { ...state, [context]: [...files] };
+    state[context] = [...files];
   },
 
   DELETE_FILES: (state, { context, fileId }) => {
-    state = {
-      ...state,
-      [context]: [...state[context].filter((file) => file.id !== fileId)],
-    };
+    state[context] = [...state[context].filter((file) => file.id !== fileId)];
   },
 };
