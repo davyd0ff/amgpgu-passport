0a2ba5cc711f7c34681060165389e2a14e587f53
Ark Davyd0ff <ark.davyd0ff@gmail.com>
2022-03-10 14:43:29 +1000

    Добавил ru-локали для проекта

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Учётная запись не найдена в системе.',
+    'throttle' => 'Слишком много попыток входа в систему. Попробуйте снова через :seconds сек.',
+];
